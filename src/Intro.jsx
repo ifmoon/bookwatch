8db3def2da07560b02cf6f40be515af42feab7dd
ifmoon <ifmoon.io@gmail.com>
2021-09-05 16:23:35 +0900

    add animation

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,11 +1,14 @@
 import Desc from './IntroDesc'
-
+import { useSpring, animated } from 'react-spring'
 const Background = () => {
-
+    const props = useSpring({from: { rotateZ: 0, opacity: 0 }, to: { rotateZ: 360, opacity: 1 },config: {duration:1500}})
     return <div style={{display: 'flex'}}>
+        <animated.div style={props}>
         <div style={{position:'relative', width: 500, height: 500}}>
             <img src={require(`./resource/background.png`).default} alt='backgroud' style={{position:'absolute', width: '100%', height: '100%'}}/>
         </div>     
+        </animated.div>
+
         <Desc />
     </div>
 }
--- src/IntroDesc.jsx
+++ src/IntroDesc.jsx
@@ -1,13 +1,30 @@
-
+import { useSpring,  animated } from 'react-spring'
 
 const Desc = () => {
+    const title = useSpring({ from: {opacity: 0}, to: {opacity: 1}, delay: 500,config: {duration: 500}})
+    const p1 = useSpring({ from: {opacity: 0}, to: {opacity: 1}, delay: 1000,config: {duration: 500}})
+    const p2 = useSpring({ from: {opacity: 0}, to: {opacity: 1},delay: 1500, config: {duration: 500}})
+    const p3 = useSpring({ from: {opacity: 0}, to: {opacity: 1},delay: 2000, config: {duration: 500}})
+    const p4 = useSpring({ from: {opacity: 0}, to: {opacity: 1},delay: 3000, config: {duration: 500}})
+    
     return <div style={{marginLeft:12}}>
         <div style={{width: '40vw', display: 'flex', flexDirection: 'column', 'alignItems':'center'}}>
+        <animated.div style={title}>
             <p style={{fontWeight: 700, fontSize: '18pt'}}>B O O K W A T C H</p>
+        </animated.div>
+        <animated.div style={p1}>
             <p style={{fontWeight: 400, fontSize: '14pt'}}>마법사,</p>
+        </animated.div>
+        <animated.div style={p2}> 
             <p style={{fontWeight: 400, fontSize: '14pt'}}>서적경,</p>
+        </animated.div>
+        <animated.div style={p3}>  
             <p style={{fontWeight: 400, fontSize: '14pt'}}>여신,</p>
+        </animated.div>
+        <animated.div style={p4}>
             <p style={{fontWeight: 500, fontSize: '14pt'}}>그리고 사람.</p>
+        </animated.div>
+        
         </div>
     </div>
 }
