ba5c0c75db028536b4420d21d9caec8fa3e9c004
ifmoon <ifmoon.io@gmail.com>
2021-09-05 20:16:30 +0900

    button \

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import BackgroundWhite from './BackgroundWhite';
 import BackgroundBlack from './BackgroundBlack';
-import {Switch, Route, Link} from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import BackgroundBrown from './BackgroundBrown';
 import Intro from './Intro'
 import './App.css'
 
 function App() {
   return (
-    <div style={{minWidth: 1000}}>
+    <div style={{ minWidth: 1000 }}>
       <Switch>
         <Route path="/white">
-          <BackgroundWhite/>
+          <BackgroundWhite />
         </Route>
         <Route path="/black">
           <BackgroundBlack />
@@ -19,16 +19,15 @@ function App() {
           <BackgroundBrown />
         </Route>
         <Route path="/">
-          <Intro/>
+          <Intro />
         </Route>
       </Switch>
-      <div style={{position: 'fixed', bottom: 0, left: 0,display: 'flex', height: 50,width: 500, justifyContent: 'space-around',alignItems: 'center'}}>
-        <Link to={"/white"} style={{fontSize: '18pt'}}>사라스바티</Link>
-        <Link to={"/black"} style={{fontSize: '18pt'}}>크투가</Link>
-        <Link to={"/brown"} style={{fontSize: '18pt'}}>페르세포네</Link>
+      <div style={{ position: 'fixed', bottom: 0, left: 0, display: 'flex', height: 50, width: 500, justifyContent: 'space-around', alignItems: 'center' }}>
+        <Link to={"/white"} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',fontSize: '18pt', color: 'white', width: 150, height: 80, textDecoration: 'none', borderRadius: 20,backgroundColor: '#636363',background: 'linear-gradient(145deg, #6a6a6a, #595959);', boxShadow: '3px 3px 4px #454545, -3px -2px 4px #818181'}}>사라스바티</Link>
+        <Link to={"/black"} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '18pt', color: 'white', width: 150, height: 80, textDecoration: 'none', borderRadius: 20,backgroundColor: '#636363',background: 'linear-gradient(145deg, #6a6a6a, #595959);',boxShadow: '3px 3px 4px #454545, -3px -3px 4px #818181'}}>크투가</Link>
+        <Link to={"/brown"} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',fontSize: '18pt', color: 'white', width: 150, height: 80, textDecoration: 'none', borderRadius: 20,backgroundColor: '#636363',background: 'linear-gradient(145deg, #6a6a6a, #595959);', boxShadow: '3px 3px 4px #454545, -3px -3px 4px #818181'}}>페르세포네</Link>
       </div>
     </div>
   );
 }
-
 export default App;
